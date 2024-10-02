4693a56e3705b3c67f8b0dea10ca2334ceae3e3e
tellang <tellang@daum.net>
2024-10-02 12:30:31 +0900

    :sparkles: 사용자 DTO와 RoleType Enum 정의 (#23)
    - 공통 모듈 적용시 통합
    - UserDto를 통해 사용자 정보 전송을 위한 객체 생성
    - RoleType Enum 정의, 사용자 권한 관리 목적
    - RoleType을 기반으로 사용자 권한 로직에 사용될 예정
    
    Signed-off-by: tellang <tellang@daum.net>

---
+++ auth-service/src/main/java/com/ticketing/authservice/infrastructure/common/RoleType.java
@@ -0,0 +1,40 @@
+package com.ticketing.authservice.infrastructure.common;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum RoleType {//TODO: 공통모듈 적용시 통합
+
+	USER(Authority.USER),
+	MANAGER(Authority.MANAGER),
+	MASTER(Authority.MASTER);
+
+	private final String authority;
+
+	/**
+	 * roleCode에 따른 RoleType을 반환
+	 *
+	 * @param roleCode 전달 받은 권한(Role_권한)형식
+	 * @return RoleType
+	 */
+	public static RoleType fromRoleCode(String roleCode) {
+		for (RoleType role : RoleType.values()) {
+			if (role.authority.equalsIgnoreCase(roleCode)) {
+				return role;
+			}
+		}
+		throw new IllegalArgumentException("Invalid role code: " + roleCode);
+	}
+
+	public String getAuthority() {
+		return this.authority;
+	}
+
+	public static class Authority {
+		public static final String USER = "ROLE_USER";
+		public static final String MANAGER = "ROLE_MANAGER";
+		public static final String MASTER = "ROLE_MASTER";
+	}
+
+}
+

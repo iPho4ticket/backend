ca67cc597a109ba61458fcd596f2de93069ea719
SerenityZenDev <tjrqo2000@gmail.com>
2024-10-02 16:44:47 +0900

    :bug: 경로 수정

--- notification-service/src/main/java/com/ipho4ticket/notificationservice/infrastructure/configuration/JpaConfig.java
+++ notification-service/src/main/java/com/ipho4ticket/notificationservice/infrastructure/configuration/JpaConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories("com.ipho4ticket.paymentservice.infrastructure.repository")
+@EnableJpaRepositories("com.ipho4ticket.notificationservice.domain.repository")
 public class JpaConfig {
 
 }

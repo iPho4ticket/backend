cf29e7b44a7b069550bc77aa5b49b44c5257567d
tellang <tellang@daum.net>
2024-10-08 18:06:45 +0900

    :sparkles: 내부 통신 서비스를 위한 응답 레코드 및 DTO 매핑 메소드 추가 (#62)
    - 추후 통합모듈로 일괄 리펙토링 예정
    - `UserDto` 인터페이스에 `Auth` 관련 요청/응답을 위한 인터페이스 추가
      - `Auth.Result` 레코드를 통한 `Auth` 서비스에서의 로그인시 데이터 확인을 위한 DTO 추가
    - `UserMapper`에 `authDtoFrom` 메소드 추가하여 `User` 객체에서 `Auth.Result`로 변환 가능하도록 함
    
    Signed-off-by: tellang <tellang@daum.net>

--- user-service/src/main/java/com/ticketing/userservice/application/dto/UserDto.java
+++ user-service/src/main/java/com/ticketing/userservice/application/dto/UserDto.java
@@ -7,7 +7,27 @@ import com.ticketing.userservice.infrastructure.common.RoleType;
 import lombok.Builder;
 import lombok.With;
 
-public interface UserDto {
+public interface UserDto { //TODO: 공통모듈 적용시 통합
+
+	/**
+	 * Auth 관련 요청/응답을 담는 인터페이스입니다.
+	 */
+	interface Auth {
+
+		/**
+		 * 로그인 결과를 반환하는 DTO입니다. (readUserByEmail의 결과로 사용)
+		 *
+		 * @param id 사용자 ID
+		 * @param name 사용자 이름
+		 * @param email 사용자 이메일 주소
+		 * @param password 사용자 비밀번호 (해시된 비밀번호)
+		 * @param role 사용자 권한 정보
+		 */
+		@With
+		@Builder
+		record Result(Long id, String name, String email, String password, RoleType role) {
+		}
+	}
 
 	interface Delete {
 		@With

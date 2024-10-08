ef6fa28d4ee982f518b05bbe404b80ede79da1de
juoonge <juoonge7646@naver.com>
2024-10-08 10:06:51 +0900

    :fire: feign모듈 구조 변경

---
+++ client-event-feign/src/main/java/com/ipho4ticket/clienteventfeign/EventConfig.java
@@ -0,0 +1,9 @@
+package com.ipho4ticket.clienteventfeign;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class EventConfig {
+}

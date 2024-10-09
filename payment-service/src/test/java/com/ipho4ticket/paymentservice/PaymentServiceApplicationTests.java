8193fc5c06eb76ef5d0cad1440483d1fc26de786
Skadi <74538745+SerenityZenDev@users.noreply.github.com>
2024-10-09 14:01:03 +0900

    ✨ dockerfile 설정 & feign 설정 (#56)
    * :memo: dockerfile 생성
    
    * :sparkles: feign 관련 설정

--- payment-service/src/test/java/com/ipho4ticket/paymentservice/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/ipho4ticket/paymentservice/PaymentServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.ipho4ticket.paymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaymentServiceApplicationTests {
 
 	@Test

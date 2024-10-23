b7c3f37a924a178f5ed7e4b767c5805f3d242115
CZ-punk <cnhhw0408@gmail.com>
2024-10-23 16:43:28 +0900

    :lock: ticket-service SecurityUtil 사용, test code 수정

--- ticket-service/src/main/java/com/ipho/ticketservice/presentation/controller/TicketController.java
+++ ticket-service/src/main/java/com/ipho/ticketservice/presentation/controller/TicketController.java
@@ -2,6 +2,7 @@ package com.ipho.ticketservice.presentation.controller;
 
 import com.ipho.ticketservice.presentation.request.TicketRequestDto;
 import com.ipho.ticketservice.application.service.TicketService;
+import com.ticketing.authzfilter.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,24 +18,26 @@ import static com.ticketing.authzfilter.infrastructure.common.RoleType.Authority
 public class TicketController {
 
     private final TicketService ticketService;
-    // 인증/인가는 방법은 추후 논의
 
     @PreAuthorize("hasRole('"+ USER +"')")
     @PostMapping
     public ResponseEntity<?> reservationTicket(@RequestBody TicketRequestDto ticketRequestDto) {
-        return ResponseEntity.ok(ticketService.reservationTicket(ticketRequestDto));
+        Long userId = SecurityUtil.getUserId();
+        return ResponseEntity.ok(ticketService.reservationTicket(ticketRequestDto, userId));
     }
 
     @PreAuthorize("hasRole('"+ USER +"')")
     @GetMapping("/{ticketId}")
     public ResponseEntity<?> searchTicketInfo(@PathVariable UUID ticketId) {
-        return ResponseEntity.ok(ticketService.searchTicketInfo(ticketId));
+        Long userId = SecurityUtil.getUserId();
+        return ResponseEntity.ok(ticketService.searchTicketInfo(ticketId, userId));
     }
 
     @PreAuthorize("hasRole('"+ USER +"')")
     @PutMapping("/{ticketId}/cancel")
     public ResponseEntity<?> cancelTicket(@PathVariable UUID ticketId) {
-        return ResponseEntity.ok(ticketService.cancelTicket(ticketId));
+        Long userId = SecurityUtil.getUserId();
+        return ResponseEntity.ok(ticketService.cancelTicket(ticketId, userId));
     }
 
 }

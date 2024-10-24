1926a9acf9cf46031e50984f91481dd8eee318f6
CZ-punk <cnhhw0408@gmail.com>
2024-10-24 18:39:12 +0900

    ecr test - ticket scanPackages 수정

--- ticket-service/src/main/java/com/ipho/ticketservice/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/ipho/ticketservice/TicketServiceApplication.java
@@ -3,7 +3,7 @@ package com.ipho.ticketservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ipho.ticketservice", "com.ticketing.authzfilter"})
 public class TicketServiceApplication {
 
     public static void main(String[] args) {
